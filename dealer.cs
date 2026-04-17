91caff5b52ebc2fc56df57c8984b781fda8b15be
Joe74012 <1210764@student.roc-nijmegen.nl>
2026-04-17 21:25:58 +0200

    Dealer class toegevoegd.

---
+++ dealer.cs
@@ -0,0 +1,24 @@
+﻿using blackjackOOP;
+
+public class Dealer
+{
+    public List<Card> Hand { get; set; }
+
+    public Dealer()
+    {
+        Hand = new List<Card>();
+    }
+
+    public int HandValue()
+    {
+        int total = 0;
+        foreach (Card card in Hand)
+            total += card.Value;
+        return total;
+    }
+
+    public bool MoetHitten()
+    {
+        return HandValue() < 17;
+    }
+}
